a26f260637288edb4334bb4505f101aa15e7b266
Taslim Khan <khandonald71@gmail.com>
2022-05-30 15:52:57 +0530

    updated for loop and again commiting

--- for_loops.java
+++ for_loops.java
@@ -1,3 +1,8 @@
-for (int i=0; i<5; i++) {
-	System.out.println("Taslim");
+class Main{
+	public static void main(String[] args) {
+		for (int i=0; i<5; i++)
+			System.out.println("Taslim");
+		System.out.println("Done with for loops");
+		System.out.println("Something new has been done");
+	}
 }
